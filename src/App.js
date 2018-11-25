4e42d264491f91d01daeff89a9f334eb5e295f38
Jake Worth <jake@jakeworth.com>
2018-11-25 14:33:10 -0600

    Refactor with useState hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './normalize.css';
-import React from 'react';
+
+import React, { useState } from 'react';
 
 const App = () => (
   <div
@@ -14,51 +15,48 @@ const App = () => (
   </div>
 );
 
-class EqualityChart extends React.Component {
-  constructor(props) {
-    super(props);
+const axisValues = () => [
+  true,
+  false,
+  1,
+  0,
+  -1,
+  'true',
+  'false',
+  '1',
+  '0',
+  '-1',
+  '',
+  null,
+  undefined,
+  Infinity,
+  -Infinity,
+  [],
+  {},
+  [[]],
+  [0],
+  [1],
+  NaN,
+];
 
-    const comparatorArray = this.axis().map(() => this.axis().slice(0));
-    const dataModel = this.axis().map((xValue, index) =>
-      comparatorArray[index].map(yValue => ({
-        // eslint-disable-next-line
-        twoquals: yValue == xValue,
-        threequals: yValue === xValue,
-      }))
-    );
+const createDataModel = () => {
+  const comparatorArray = axisValues().map(() => axisValues().slice(0));
 
-    this.state = {
-      axis: this.axis(),
-      dataModel,
-      view: 'twoquals',
-    };
-  }
+  return axisValues().map((xValue, index) =>
+    comparatorArray[index].map(yValue => ({
+      // eslint-disable-next-line
+      twoquals: yValue == xValue,
+      threequals: yValue === xValue,
+    }))
+  );
+};
 
-  axis = () => [
-    true,
-    false,
-    1,
-    0,
-    -1,
-    'true',
-    'false',
-    '1',
-    '0',
-    '-1',
-    '',
-    null,
-    undefined,
-    Infinity,
-    -Infinity,
-    [],
-    {},
-    [[]],
-    [0],
-    [1],
-    NaN,
-  ];
+const EqualityChart = () => {
+  const [dataModel] = useState(createDataModel);
+  const [view, setView] = useState('twoquals');
+  const [axis] = useState(axisValues);
 
-  displayName = value => {
+  const displayName = value => {
     if (
       value instanceof Object ||
       value instanceof Array ||
@@ -71,104 +69,82 @@ class EqualityChart extends React.Component {
     }
   };
 
-  viewIcon = view => (view === 'twoquals' ? '==' : '===');
+  const viewIcon = currentView => (currentView === 'twoquals' ? '==' : '===');
 
-  render() {
-    const { axis, dataModel, view } = this.state;
-
-    return (
-      <div style={{ margin: 'auto', width: '600px' }}>
-        <div style={{ marginBottom: '60px' }}>
-          <h2 style={{ textTransform: 'capitalize' }}>
-            {view} (<code>{this.viewIcon(view)}</code>)
-          </h2>
-        </div>
-        <table style={{ marginBottom: '20px' }}>
-          <thead>
-            <tr>
-              <td />
-              {axis.map((value, index) => (
-                <th
-                  scope="col"
+  return (
+    <div style={{ margin: 'auto', width: '600px' }}>
+      <div style={{ marginBottom: '60px' }}>
+        <h2 style={{ textTransform: 'capitalize' }}>
+          {view} (<code>{viewIcon(view)}</code>)
+        </h2>
+      </div>
+      <table style={{ marginBottom: '20px' }}>
+        <thead>
+          <tr>
+            <td />
+            {axis.map((value, index) => (
+              <th
+                scope="col"
+                key={index}
+                style={{
+                  transform: 'rotate(-90deg)',
+                  maxWidth: '20px',
+                  maxHeight: '20px',
+                }}
+              >
+                {displayName(value)}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dataModel.map((row, index) => (
+            <tr key={index}>
+              <th scope="row">{displayName(axis[index])}</th>
+              {row.map((cell, index) => (
+                <td
                   key={index}
                   style={{
-                    transform: 'rotate(-90deg)',
-                    maxWidth: '20px',
-                    maxHeight: '20px',
+                    background: cell[view] ? '#654ea3' : '#fff',
+                    width: '20px',
+                    height: '20px',
+                    border: '1px solid #d3d3d3',
                   }}
-                >
-                  {this.displayName(value)}
-                </th>
+                />
               ))}
             </tr>
-          </thead>
-          <tbody>
-            {dataModel.map((row, index) => (
-              <tr key={index}>
-                <th scope="row">
-                  {this.displayName(axis[index])}
-                </th>
-                {row.map((cell, index) => (
-                  <td
-                    key={index}
-                    style={{
-                      background: cell[view] ? '#654ea3' : '#fff',
-                      width: '20px',
-                      height: '20px',
-                      border: '1px solid #d3d3d3',
-                    }}
-                  />
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+          ))}
+        </tbody>
+      </table>
 
-        <div style={{ marginBottom: '20px' }}>
-          <button onClick={() => this.setState({ view: 'twoquals' })}>
-            Twoquals
-          </button>
-          <button onClick={() => this.setState({ view: 'threequals' })}>
-            Threequals
-          </button>
-        </div>
-
-        <div style={{ marginBottom: '20px' }}>
-          <p>
-            This application shows the JavaScript value-comparison operators
-            {' '}
-            <code>==</code>
-            {' '}
-            and
-            {' '}
-            <code>===</code>
-            {' '}
-            in action. It is written in React.js and inspired by the
-            {' '}
-            <a href="https://dorey.github.io/JavaScript-Equality-Table/">
-              JavaScript Equality Table
-            </a>.
-          </p>
-          <p>
-            To quote Douglas Crawford: "My advice is to never use the evil twins. Instead, always use
-            {' '}
-            <code>===</code>
-            {' '}
-            and
-            {' '}
-            <code>!==</code>
-            ."
-          </p>
-          <p>
-            <a href="https://github.com/jwworth/javascript-equality">
-              Source code
-            </a>
-          </p>
-        </div>
-
+      <div style={{ marginBottom: '20px' }}>
+        <button onClick={() => setView('twoquals')}>Twoquals</button>
+        <button onClick={() => setView('threequals')}>Threequals</button>
       </div>
-    );
-  }
-}
+
+      <div style={{ marginBottom: '20px' }}>
+        <p>
+          This application shows the JavaScript value-comparison operators{' '}
+          <code>==</code> and <code>===</code> in action. It is written in
+          React.js and inspired by the{' '}
+          <a href="https://dorey.github.io/JavaScript-Equality-Table/">
+            JavaScript Equality Table
+          </a>
+          .
+        </p>
+        <p>
+          To quote Douglas Crawford: "My advice is to never use the evil twins.
+          Instead, always use <code>===</code> and <code>!==</code>
+          ."
+        </p>
+        <p>
+          <a href="https://github.com/jwworth/javascript-equality">
+            Source code
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
